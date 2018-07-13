e377e2c8deefaf9da922231edf470a9d5bff15ed
Karla R <karla.y.reyes12@gmail.com>
2018-07-13 08:08:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,21 +9,9 @@ def countdown(x)
 end
 
 def countdown_with_sleep(y)
-  while y >= 
-end
-
-def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
+  while y > 0
+    puts "#{y} SECOND(S)!"
+    y -= 1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
